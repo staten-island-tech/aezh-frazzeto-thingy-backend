5d755ca7b7f1f28d3d2abd00ec2e8215c91ac8ed
zijie28 <196802554+zijie28@users.noreply.github.com>
2026-08-02 21:44:45 -0400

    debuging time 9x

--- backend/frazzetoBookReview/models.py
+++ backend/frazzetoBookReview/models.py
@@ -9,13 +9,13 @@ class UserProfile(models.Model):
     is_instructor = models.BooleanField(default=False)
 
 class Authors(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.TextField()
 
 class Books(models.Model):
     author = models.ForeignKey(Authors,on_delete=models.CASCADE,related_name="books")
     img = models.URLField()
     genre = models.CharField(max_length=40)
-    title = models.CharField(max_length=100)
+    title = models.TextField()
     pageLength = models.IntegerField()
 
 class Courses(models.Model):
