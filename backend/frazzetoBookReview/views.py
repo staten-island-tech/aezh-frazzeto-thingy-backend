321812ce4c55b12d03be1aa89bed2d6942721943
zijie28 <196802554+zijie28@users.noreply.github.com>
2026-08-02 23:49:34 -0400

    id search for books 2x

--- backend/frazzetoBookReview/views.py
+++ backend/frazzetoBookReview/views.py
@@ -69,7 +69,7 @@ class BookView(generics.ListCreateAPIView):
         title = self.request.query_params.get("title")
         genre = self.request.query_params.get("genre")
         author = self.request.query_params.get("author")
-        bood_id = self.request.query_params.get("id")
+        book_id = self.request.query_params.get("id")
         if title:
             queryset = queryset.filter(title__icontains=title)
         if genre:
